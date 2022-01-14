3ebdaa01401293b13183d53ba86a837873c994b6
Hyper-Viper <71551888+Hyper-Viper@users.noreply.github.com>
2022-01-14 06:20:02 +0530

    Update kwitter_page.js

--- kwitter_page.js
+++ kwitter_page.js
@@ -22,15 +22,17 @@ function send()
 {
   msg_likers = [user_name]
   msg = document.getElementById("msg").value;
-  firebase.database().ref(room_name).push({
-    name:user_name,
-    message:msg,
-    message_num:msg_num,
-    likers:msg_likers,
-    path:svg_path
-  });
+  if (msg.trim().length != 0){
+    firebase.database().ref(room_name).push({
+      name:String(user_name),
+      message:String(msg),
+      message_num:msg_num,
+      likers:Array(msg_likers),
+      path:String(svg_path)
+    });
+    getData();
+  }
   document.getElementById("msg").value = "";
-  getData();
 }
 
 function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
@@ -81,8 +83,8 @@ likes = document.getElementById(button_id).value;
     svg_path = "M18.77,11h-4.23l1.52-4.94C16.38,5.03,15.54,4,14.38,4c-0.58,0-1.14,0.24-1.52,0.65L7,11H3v10h4h1h9.43 c1.06,0,1.98-0.67,2.19-1.61l1.34-6C21.23,12.15,20.18,11,18.77,11z M7,20H4v-8h3V20z M19.98,13.17l-1.34,6 C18.54,19.65,18.03,20,17.43,20H8v-8.61l5.6-6.06C13.79,5.12,14.08,5,14.38,5c0.26,0,0.5,0.11,0.63,0.3 c0.07,0.1,0.15,0.26,0.09,0.47l-1.52,4.94L13.18,12h1.35h4.23c0.41,0,0.8,0.17,1.03,0.46C19.92,12.61,20.05,12.86,19.98,13.17z";
   }
   firebase.database().ref(room_name).child(message_id).update({
-    likers : msg_likers,
-    path : svg_path
+    likers : Array(msg_likers),
+    path : String(svg_path)
   });
 }
 
